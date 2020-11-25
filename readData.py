eeb6eda83f8a52ae6f3bed2d99b1c966b21b57fa
praneeth <praneethkrishna27@hotmail.com>
2020-11-25 18:50:05 +0530

    Added date range selector and stock ticker

--- readData.py
+++ readData.py
@@ -1,17 +1,46 @@
 import csv
-
 import yfinance as yf
+import streamlit as st
+import numpy as np
+import pandas as pd
+import sys
+import datetime
+import time
 
-def get_data(ticker_list):
+def get_data(ticker_list, from_date, to_date):
     """    
-    function to get data 
+    function to get data as per the ticker selected within the specified time ran
     """
-    data = yf.download(tickers=' '.join(ticker_list),
-                       period='ytd',
-                       group_by='ticker',
-                       auto_adjust = True,
-                       prepost = True,
-                       threads = True,)
+    try:
+        data = yf.download(tickers=ticker_list,
+                        period='ytd',
+                        group_by='ticker',
+                        auto_adjust = True,
+                        prepost = True,
+                        threads = True,)
+
+        data = data[from_date:to_date]
+    except:
+        print(sys.exc_info()[0])
     return(data)
-lst = ['SPY', 'AAPL']
-data = get_data(lst) 
+
+
+
+
+
+if __name__ == '__main__':
+
+    st.write('Hello welcome to the Stock Market Analytics Web App!!')
+    # data = get_data(['AAPL'])
+    # st.write(data)
+    # change this code to select multiple stocks
+    today = datetime.date.today()
+    def_start_date = time.strftime('%Y-%m-1')
+    def_start_date = datetime.datetime.strptime(def_start_date, '%Y-%m-%d')
+    option = st.selectbox('Please choose the stock tickers you are interested to Analyze', ['SPY', 'AAPL', 'AMZN'])
+    from_date = st.date_input('from date', (def_start_date))
+    to_date = st.date_input('to date', )
+    # st.write(f'you have selected {}')
+    st.write('You have selected:', option)
+    data = get_data(''.join(option), from_date, to_date)
+    st.write(data)
